3d67f21eb9cab46d0dc334511e1328dc46302649
flow2708 <dibirovamin10@gmail.com>
2025-05-04 13:32:19 +0300

    Update Main.java

--- src/main/java/ru/flow/httpserver/core/Main.java
+++ src/main/java/ru/flow/httpserver/core/Main.java
@@ -5,6 +5,6 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException {
         HttpServer httpServer = new HttpServer();
-        httpServer.start()
+        httpServer.start();
     }
 }
