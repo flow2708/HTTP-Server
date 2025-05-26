c3e597e38820fbb79aaae9705d4e4cd7699f2187
flow2708 <dibirovamin10@gmail.com>
2025-05-27 00:30:10 +0300

    Create HtmlBuilderService.java
    +renderUserPostsList

---
+++ src/main/java/ru/flow/httpserver/services/HtmlBuilderService.java
@@ -0,0 +1,11 @@
+package ru.flow.httpserver.services;
+
+import ru.flow.httpserver.dao.SQLite;
+
+import java.io.PrintWriter;
+
+public class HtmlBuilderService {
+    public static final void renderUserPostsList(PrintWriter out, SQLite db, String username) {
+
+    }
+}
